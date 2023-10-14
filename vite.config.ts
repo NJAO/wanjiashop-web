69bcfc02fd008e02b5532d0020eb534c71beb16d
NJAO <1157515428@qq.com>
2023-10-15 02:24:02 +0800

    fix: config - auto ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,54 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from "vite"
+import { resolve } from "path"
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
+import Components from "unplugin-vue-components/vite"
+import vue from "@vitejs/plugin-vue"
+import AutoImport from "unplugin-auto-import/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),AutoImport({
-    dts:true,
-    include:[/\.[tj]sx?$/, /\.vue$/],
-    imports: [
-      'vue',
-      'vue-router',
-      {
-        '@vueuse/core': [
-          // named imports
-          'useMouse', // import { useMouse } from '@vueuse/core',
-          // alias
-          ['useFetch', 'useMyFetch'] // import { useFetch as useMyFetch } from '@vueuse/core',
-        ],
-        axios: [
-          // default imports
-          ['default', 'axios'] // import { default as axios } from 'axios',
-        ]
-      }
-    ],
-    resolvers:[AntDesignVueResolver()],
-    eslintrc: {
-      enabled: true
-    }
-  }),
-  Components({
-    resolvers: [AntDesignVueResolver({importStyle: true, resolveIcons: true})]
-  })],
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: true,
+      include: [/\.[tj]sx?$/, /\.vue$/],
+      imports: [
+        "vue",
+        "vue-router",
+        {
+          "@vueuse/core": [
+            // named imports
+            "useMouse", // import { useMouse } from '@vueuse/core',
+            // alias
+            ["useFetch", "useMyFetch"], // import { useFetch as useMyFetch } from '@vueuse/core',
+          ],
+          axios: [
+            // default imports
+            ["default", "axios"], // import { default as axios } from 'axios',
+          ],
+        },
+      ],
+      resolvers: [AntDesignVueResolver()],
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({ importStyle: true, resolveIcons: true }),
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      "@": resolve('./src')
-    }
+      "@": resolve("./src"),
+    },
   },
-  css:{
+  css: {
     preprocessorOptions: {
       scss: {
-        additionalData:'@import "@/assets/scss/global.scss";'
-      }
-    }
-  }
+        additionalData: '@import "@/assets/scss/global.scss";',
+      },
+    },
+  },
 })
